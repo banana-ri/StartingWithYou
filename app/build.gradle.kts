b0d170b3a8495fde64e4f4a6b5d6280742f4cf47
uingEng <uingEng@users.noreply.github.com>
2026-02-11 16:35:22 +0900

    레이아웃 수정, 칩 세팅, fab 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,6 +42,8 @@ dependencies {
     implementation(libs.constraintlayout)
     implementation(libs.navigation.fragment)
     implementation(libs.navigation.ui)
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.android.support:design:26.1.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
